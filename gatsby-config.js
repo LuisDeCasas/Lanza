73b054157184e2c21d27ac090107a4a8296343f1
Luis De Casas <luisdecasas@Luiss-MacBook-Pro.local>
2021-03-22 22:15:38 -0500

    Title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lanza Transportation`,
+    description: `Lanza Transportation and Towing Services for Newburgh and surroundings`,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
